281aa1ce278a14402d4ec7402514d1a3bee75045
bohadi <bohadi@users.noreply.github.com>
2017-07-25 19:34:27 -0500

    add pip conf

---
+++ symlink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dir=~/dotfiles                    
+files="vimrc zshrc inputrc haskeline gitconfig crawlrc tmux.conf"
+
+cd $dir
+for file in $files; do
+    ln -s $dir/$file ~/.$file
+done
